7e7642a4b9fab6964881f6af4afacde9d9fc753e
Chad <cedie@hotmail.com>
2013-12-03 16:13:24 -0800

    added twitter bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 VapeApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/sign_in',  to: 'static_pages#sign_in', via: 'get'
+  match '/about_us', to: 'static_pages#about_us', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,29 +3,34 @@ require 'spec_helper'
 describe "StaticPages" do
 
   let(:base_title) {'California Vapor Reserve'}
-  
+
   describe "Home page" do
 
   	it "has the content 'California Vapor Reserve'" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).to have_content('Home')
     end
 
-    it "has the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+    it "has the base title" do
+      visit root_path
+      expect(page).to have_title("California Vapor Reserve")
+    end
+
+    it "does not have the custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
 
     it "has the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "has the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -33,25 +38,25 @@ describe "StaticPages" do
   describe "About page" do
 
     it "has the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "has the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
 
-    it "has the content 'cedie@hotmail.com'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('cedie@hotmail.com')
+    it "has the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
     end
 
     it "has the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
