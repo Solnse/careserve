294ac19bc906698ce45cb41f12b75900fb86ae54
Chad <cedie@hotmail.com>
2013-12-05 17:40:46 -0800

    finish sessions sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to California Vapor Reserve."
       redirect_to @user
     else
