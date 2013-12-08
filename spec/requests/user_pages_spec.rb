41f3e9ae521cfa7a003abe0b0caa6f293f44409a
Chad <cedie@hotmail.com>
2013-12-07 17:31:06 -0800

    added user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -58,10 +58,19 @@ describe "UserPages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:mp1) { FactoryGirl.create(:micropost, user: user, content: "lallala?") }
+    let!(:mp2) { FactoryGirl.create(:micropost, user: user, content: "ahahahaha!") }
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do 
+      it { should have_content(mp1.content) }
+      it { should have_content(mp2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup" do 
@@ -130,7 +139,7 @@ describe "UserPages" do
         fill_in "Name", with: new_name
         fill_in "Email", with: new_email
         fill_in "Password", with: user.password
-        fill_in "Confirm Password", with: user.password
+        fill_in "Confirmation", with: user.password
         click_button "Save changes"
       end
 
@@ -140,5 +149,18 @@ describe "UserPages" do
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
     end
+
+    describe "forbidden attributes" do 
+      let(:params) do 
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do  
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
   end
+
 end
